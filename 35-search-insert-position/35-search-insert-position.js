8dc38db3a112ebb6a2d164fab516ec1958a570c3
Alex Fernández <80935923+Arekkusu17@users.noreply.github.com>
2022-02-11 23:51:47 -0300

    Time: 87 ms (50.85%), Space: 42.5 MB (8.36%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -5,12 +5,13 @@
  */
 var searchInsert = function(nums, target) {
     
-    let left=0,right=nums.length
+    let left=0,right=nums.length;
     
     while (left < right) {
         const middle = Math.floor((left + right) / 2);
-        const current=nums[middle]
-
+        const current=nums[middle];
+        
+        // 
         if (current < target) {
             left = middle + 1;
         } else {
