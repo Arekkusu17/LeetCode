1924b8db84698a8431d57253f049516533305bfa
Alex Fernández <80935923+Arekkusu17@users.noreply.github.com>
2022-02-12 00:14:35 -0300

    Time: 125 ms (39.14%), Space: 44.4 MB (40.26%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(str) {
+	let newString = str.replace(/[\W_]+/g, '').toLowerCase();
+	console.log(newString);
+
+	for (let i = 0; i < newString.length; i++) {
+		if (newString[i] !== newString[newString.length - 1 - i]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+
+    
