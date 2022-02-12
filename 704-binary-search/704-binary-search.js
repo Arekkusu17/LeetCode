4180bf0b9fb541da11ab5feae414fc75f7fb9025
Alex Fernández <80935923+Arekkusu17@users.noreply.github.com>
2022-02-11 22:47:22 -0300

    Time: 64 ms (97.66%), Space: 45.4 MB (8.40%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -4,11 +4,21 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    for(let i=0;i<nums.length;i++){
-        if(nums[i]===target){
-            return i
-        }
-    }return -1;
+    let left=0;
+    let right=nums.length-1
     
+    while (left<=right){
+        const middle = Math.floor( ((left + right) / 2) );
+        const current=nums[middle];     
     
+        if (current>target){
+          right=middle-1;
+          } else if(current<target){
+             left=middle+1
+         }else return middle;
+      }
+    
+    return -1;
 };
+    
+    
