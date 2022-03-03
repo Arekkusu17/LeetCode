acda9324c0d36410f5a037e0c070b1433510a5ad
Alex Fernández <80935923+Arekkusu17@users.noreply.github.com>
2022-03-02 21:55:01 -0300

    Time: 56 ms (97.65%), Space: 42.5 MB (10.24%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let arr=s.split(" ").filter(word=>word.length>0);
+    return arr[arr.length-1].length;
+    
+};
